752c8096373a33a36afcb25183af7ac7e024dfbb
Open Audio Search team <oas@cbadevel.fro.at>
2020-10-29 18:06:52 +0100

    support ROOT_PATH environment variable for server

--- oas_core/server.py
+++ oas_core/server.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import uvicorn
+import os
 from app.server.main import app
 
+root_path = os.environ.get('ROOT_PATH') or None
+print(root_path)
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080, log_level='info')
+    uvicorn.run(app, host='0.0.0.0', port=8080, log_level='info', root_path=root_path)
