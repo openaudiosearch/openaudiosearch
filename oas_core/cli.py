733f75c5da2fbbcfa039ebadb0715d7b275f7f83
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-10-29 12:48:41 +0100

    cleanup

--- oas_core/cli.py
+++ oas_core/cli.py
@@ -4,52 +4,7 @@ from pydantic_cli import run_and_exit, to_runner
 
 from app.tasks.models import *
 
-
-# def example_runner(opts: TranscribeOpts) -> int:
-#     print(f"Mock example running with options {opts}")
-#     return 0
-
-# if __name__ == '__main__':
-#     # to_runner will return a function that takes the args list to run and 
-#     # will return an integer exit code
-#     sys.exit(to_runner(TranscribeOpts, example_runner, version='0.1.0')(sys.argv[1:]))
-
-
-
-# from pydantic import BaseModel, AnyUrl
-# from pydantic_cli.examples import ExampleConfigDefaults
 from pydantic_cli import run_sp_and_exit, SubParser
-
-
-# class AlphaOptions(BaseModel):
-
-#     class Config(ExampleConfigDefaults):
-#         CLI_EXTRA_OPTIONS = {'max_records': ('-m', '--max-records')}
-
-#     input_file: str
-#     max_records: int = 10
-
-
-# class BetaOptions(BaseModel):
-
-#     class Config(ExampleConfigDefaults):
-#         CLI_EXTRA_OPTIONS = {'url': ('-u', '--url'),
-#                              'num_retries': ('-n', '--num-retries')}
-
-#     url: AnyUrl
-#     num_retries: int = 3
-
-
-# def printer_runner(opts: T.Any):
-#     print(f"Mock example running with {opts}")
-#     return 0
-
-
-# def to_runner(sx):
-#     def example_runner(opts) -> int:
-#         print(f"Mock {sx} example running with {opts}")
-#         return 0
-#     return example_runner
 
 class TranscribeArgsOpts(TranscribeOpts, TranscribeArgs):
   pass
