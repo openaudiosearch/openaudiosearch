172fd2e16b7e627396c0da34d922e235a4bd6991
osuiowq <moreiras@informatik.uni-freiburg.de>
2020-10-31 13:45:46 +0100

    minor changes on results

--- oas_core/app/tasks/transcribe_vosk.py
+++ oas_core/app/tasks/transcribe_vosk.py
@@ -2,16 +2,22 @@ from vosk import Model, KaldiRecognizer, SetLogLevel
 import wave
 import json
 
+
 def transcribe_vosk(audio, model_path):
     model = Model(model_path)
     rec = KaldiRecognizer(model, 16000)
     results = []
     wave_frames = wave.open(audio, "rb")
+    transcript = ""
     while True:
         data = wave_frames.readframes(4000)
         if len(data) == 0:
             break
         if rec.AcceptWaveform(data):
-            rec_result = json.loads(rec.Result())
-            results.append(rec_result)
-    return  results
+            recResult = json.loads(rec.Result())
+            transcript = transcript + " " + recResult['text']
+            results.append(recResult)
+        else:
+            rec.PartialResult()
+    # TODO: Why does rec.FinalResult() not work?
+    return results
