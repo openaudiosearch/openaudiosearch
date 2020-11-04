5acefc8dafe778bd8ae8bb248346e18bf550e4ee
osuiowq <moreiras@informatik.uni-freiburg.de>
2020-11-04 16:05:38 +0100

    ensure only one running elastic client

--- oas_core/app/elastic/search.py
+++ oas_core/app/elastic/search.py
@@ -1,58 +1,86 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
 from pprint import pprint
-from app.core.job import Worker
+#from app.core.job import Worker
 import json
 
-from app.config import config
+#from app.config import config
+config = {"es_host": "localhost",
+            "es_port": "9200",
+            "index_name": "oas"}
 
-es = None
-class SearchIndex():
 
-    def __init__(self,
-                 host=config.es_host,
-                 port=config.es_port,
-                 index_name=config.index_name,
-                 ssl=False,
-                 check_certs=False,
-                 certs=""):
-        global es
-        if not es:
-            es = self
-        self.connection = Elasticsearch([host + ":"+port])
-        self.index_name = index_name
-        self.certs = certs
-        self.ssl = ssl
-        self.index = self.connection.indices.create(index=index_name, ignore=400)
-
-    def is_connected(self, host, port):
-        if self.connection != host + ":" + port:
-            return False
+class SearchIndex:
+    instance = None
+    def __init__(self, host=config["es_host"],
+                    port=config["es_port"],
+                    index_name=config["index_name"],
+                    ssl=False,
+                    check_certs=False,
+                    certs=""):
+        if not SearchIndex.instance:
+            SearchIndex.instance = SearchIndex.__SearchIndex(host,
+                port,
+                index_name,
+                ssl,
+                check_certs,
+                certs)
         else:
-            return True
-
-    def put(self, doc):
-        doc = json.dumps(doc.reprJSON(), cls=Encoder)
-        res = self.connection.index(index=self.index_name, id=id, body=doc, doc_type="_doc")
-        return res
-
-    def get(self, id):
-        self.connection.get(index=self.index_name, id=id, doc_type="_doc")
+            SearchIndex.instance.host = host
+            SearchIndex.instance.port = port
+            SearchIndex.instance.index_name = index_name
+            SearchIndex.instance.ssl = ssl
+            SearchIndex.instance.check_certs = check_certs
+            SearchIndex.instance.certs = certs
     
-    def search(self, search_term):
-        search_param = {"query": {
-            "match": {
-            "text": search_term
-            }
-        }}
-        response = self.connection.search(index=self.index_name, body=search_param, doc_type="_doc")
-        return response
-    
-    def refresh(self):
-        self.connection.indices.refresh(index=self.index_name)
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+    class __SearchIndex:
+        def __init__(self,
+                    host,
+                    port,
+                    index_name,
+                    ssl,
+                    check_certs,
+                    certs):
+            self.connection = Elasticsearch([host + ":"+port])
+            self.index_name = index_name
+            self.certs = certs
+            self.ssl = ssl
+            self.index = self.connection.indices.create(index=index_name, ignore=400)
+
+        def is_connected(self, host, port):
+            if self.connection != host + ":" + port:
+                return False
+            else:
+                return True
+
+        def put(self, doc):
+            doc = json.dumps(doc.reprJSON(), cls=Encoder)
+            res = self.connection.index(index=self.index_name, id=id, body=doc, doc_type="_doc")
+            return res
+
+        def get(self, id):
+            self.connection.get(index=self.index_name, id=id, doc_type="_doc")
+        
+        def get_con(self):
+            return self.connection
+
+        def search(self, search_term):
+            search_param = {"query": {
+                "match": {
+                "text": search_term
+                }
+            }}
+            response = self.connection.search(index=self.index_name, body=search_param, doc_type="_doc")
+            return response
+        
+        def refresh(self):
+            self.connection.indices.refresh(index=self.index_name)
 
 
-class Document():
+class Document:
     def __init__(self, asr_result, path_to_audio="to-do.mp3"):
         self.results = []
         for result in asr_result["result"]:
@@ -114,6 +142,10 @@ if __name__ == "__main__":
     path_to_audio = "path/to/audio"
     
     search_index = SearchIndex("localhost", "9200", "oas")
+    search_index2 = SearchIndex("localhost", "9200", "oas")
+
+    if search_index2.get_con() != search_index.get_con():
+        print("fuck")
     doc = Document(asr_result, path_to_audio)
 
     pprint(search_index.put(doc))
