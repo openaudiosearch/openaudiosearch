3d04ef5aeb5074e37dfeda0a4505d78ffb424fa7
Henning Schumann <henning.schumann@gmail.com>
2021-06-28 15:03:10 +0200

    Modified task-queue.py and task-status.py to work with celery. Removed task-run.py for now.

--- oas_core/task-status.py
+++ oas_core/task-status.py
@@ -4,19 +4,16 @@
 Get result from a task
 """
 
-import logging
-import sys
-import typing as T
-import json
 from argparse import ArgumentParser
 
-from app.server.jobs import jobs
+from app.server.jobs import Jobs
 
 if __name__ == "__main__":
+    jobs = Jobs()
     parser = ArgumentParser(description='Process some integers.')
     parser.add_argument('id', metavar='ID', type=str,
                         help='A job ID')
 
     args = parser.parse_args()
-    result = jobs.get_result(args.id)
-    print(json.dumps(result, indent=4))
+    result = jobs.get_job(args.id)
+    print(result)
