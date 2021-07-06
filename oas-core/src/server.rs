4d729c9c37458d479da8426cdb0f01c335958275
Franz Heinzmann (Frando) <frando@unbiskant.org>
2021-07-06 20:02:03 +0200

    change from actix_web to rocket

--- oas-core/src/server.rs
+++ oas-core/src/server.rs
@@ -1,10 +1,6 @@
 use clap::Clap;
-use std::net::{SocketAddr};
 
-
-
-use actix_web::middleware::Logger;
-use actix_web::{get, web, App, HttpRequest, HttpServer};
+use rocket::{get, routes};
 
 const DEFAULT_PORT: u16 = 8080;
 const DEFAULT_HOST: &str = "127.0.0.1";
@@ -20,32 +16,18 @@ pub struct ServerOpts {
 }
 
 pub async fn run_server(opts: ServerOpts) -> anyhow::Result<()> {
-    tokio::task::spawn_blocking(move || run_server_actix(opts)).await??;
+    let figment = rocket::Config::figment()
+        .merge(("port", opts.port.unwrap_or(DEFAULT_PORT)))
+        .merge(("address", opts.host.unwrap_or(DEFAULT_HOST.to_string())));
+
+    let app = rocket::custom(figment).mount("/hello", routes![world]);
+
+    app.launch().await?;
+
     Ok(())
 }
 
-#[actix_web::main]
-pub async fn run_server_actix(opts: ServerOpts) -> anyhow::Result<()> {
-    run_server_inner(opts).await?;
-    Ok(())
-}
-pub async fn run_server_inner(opts: ServerOpts) -> anyhow::Result<()> {
-    let host = opts.host.unwrap_or(DEFAULT_HOST.to_string());
-    let port = opts.port.unwrap_or(DEFAULT_PORT);
-    let addr: SocketAddr = format!("{}:{}", host, port).parse()?;
-
-    let _server = HttpServer::new(move || {
-        let app = App::new().wrap(Logger::default()).service(index);
-        app
-    })
-    .bind(addr)?
-    .run()
-    .await?;
-    Ok(())
-}
-
-#[get("/resource1/{name}/index.html")]
-async fn index(req: HttpRequest, name: web::Path<String>) -> String {
-    println!("REQ: {:?}", req);
-    format!("Hello: {}!\r\n", name)
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
 }
