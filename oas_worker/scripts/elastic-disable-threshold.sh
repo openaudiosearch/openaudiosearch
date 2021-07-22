1887ed439b6c445075b0c24da4def1f65b21833e
Franz Heinzmann (Frando) <frando@unbiskant.org>
2021-07-22 23:22:54 +0200

    move scripts to root dir

--- oas_worker/scripts/elastic-disable-threshold.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-ELASTIC_URL="${ELASTIC_URL:-"http://localhost:9200/"}"
-
-curl -XPUT -H "Content-Type: application/json" ${ELASTIC_URL}_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
-curl -XPUT -H "Content-Type: application/json" ${ELASTIC_URL}_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
