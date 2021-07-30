81d3653fb1402eeced17e0599bc25266c1cf75a0
Franz Heinzmann (Frando) <frando@unbiskant.org>
2021-07-30 15:33:46 +0200

    Fix docker build

--- rust/oas-core/build.rs
+++ rust/oas-core/build.rs
@@ -1,12 +1,24 @@
 use std::process::Command;
 const FRONTEND_PATH: &str = "../../frontend";
-const FRONTEND_DIST_PATH: &str = "../../frontend/dist";
 fn main() {
-    println!("cargo:rerun-if-changed={}/src", FRONTEND_PATH);
+    let frontend_path = if let Ok(path) = std::env::var("FRONTEND_PATH") {
+        path
+    } else {
+        FRONTEND_PATH.to_string()
+    };
+    let frontend_dist_path = format!("{}/{}", frontend_path, "dist");
+    println!("cargo:rerun-if-changed={}/src", frontend_path);
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed={}", "BUILD_FRONTEND");
+    println!("cargo:rerun-if-env-changed={}", "FRONTEND_PATH");
     println!("cargo:rerun-if-env-changed={}", "PROFILE");
+    println!("frontend path: {}", frontend_path);
     let build_frontend = match std::env::var("BUILD_FRONTEND") {
-        Ok(_) => true,
+        Ok(var) => match var.as_str() {
+            "" | "0" | "false" => false,
+            "1" | "true" => true,
+            _ => panic!("Invalid value for BUILD_FRONTEND variable: should be 1 or 0"),
+        },
         Err(_) => match std::env::var("PROFILE").unwrap().as_str() {
             "release" => true,
             "debug" => false,
@@ -17,7 +29,11 @@ fn main() {
         // In release mode, build the frontend using yarn. The result will be included statically
         // in the binary.
         Command::new("yarn")
-            .current_dir(FRONTEND_PATH)
+            .current_dir(&frontend_path)
+            .status()
+            .expect("Failed to install frontend dependencies");
+        Command::new("yarn")
+            .current_dir(&frontend_path)
             .arg("build")
             .status()
             .expect("Failed to build frontend");
@@ -25,6 +41,6 @@ fn main() {
         // On debug builds, don't rebuild the frontend but ensure that the directory is present.
         // Otherwise the build will fail because in src/server/mod.rs the frontend/dist directory
         // is statically included.
-        std::fs::create_dir_all(FRONTEND_DIST_PATH).expect("Failed to create frontend dist dir.");
+        std::fs::create_dir_all(&frontend_dist_path).expect("Failed to create frontend dist dir.");
     }
 }
