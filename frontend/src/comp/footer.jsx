a2d0dcd0843f6ce4dab87e59372f30a4c457fd6a
dryptomim <moreiras@informatik.uni-freiburg.de>
2021-08-15 02:29:56 +0200

    add icons to footer

--- frontend/src/comp/footer.jsx
+++ frontend/src/comp/footer.jsx
@@ -3,7 +3,7 @@ import { Flex, Box, Text, Icon, Center, Divider, Link as ChakraLink } from '@cha
 import {
   Link
 } from 'react-router-dom'
-import { FaGithub } from 'react-icons/fa'
+import { FaDiscord, FaGithub, FaInfoCircle, FaLock, FaRegNewspaper } from 'react-icons/fa'
 
 import { useTranslation } from 'react-i18next'
 import { Login } from './login'
@@ -11,7 +11,7 @@ import { Login } from './login'
 function FooterLinkText (props = {}) {
   const { children } = props
   return (
-    <Text color='gray.500' _hover={{ color: 'gray.600' }} py='3' pl='6' pr='10' fontSize='sm'>
+    <Text color='gray.500' _hover={{ color: 'gray.600' }} py='3' pl='6' pr='10' fontSize='xs'>
       {children}
     </Text>
   )
@@ -26,32 +26,43 @@ export function Footer () {
       <Center>
         <Box w={['90vw', '80vw', '750px', '750px']}>
 
-          <Flex direction={['column', 'column', 'row', 'row']} justify='start'>
-            <ChakraLink to='/imprint' as={Link}>
-              <FooterLinkText>
-                {t('imprint', 'Imprint')}
-              </FooterLinkText>
+          <Flex direction={['column', 'column', 'row', 'row']}>
+            <ChakraLink href='https://github.com/openaudiosearch/openaudiosearch' isExternal>
+              <Flex>
+                <FooterLinkText>
+                  <Icon as={FaGithub} mr='2' />
+                  Source code
+                </FooterLinkText>
+              </Flex>
+            </ChakraLink>
+            <ChakraLink href='https://discord.gg/GjdQjxrPJB' isExternal>
+              <Flex>
+                <FooterLinkText>
+                  <Icon as={FaDiscord} mr='2' />
+                  Join us on discord
+                </FooterLinkText>
+              </Flex>
             </ChakraLink>
             <ChakraLink to='/about' as={Link}>
               <FooterLinkText>
+                <Icon as={FaRegNewspaper} mr='2' />
                 {t('about', 'About us')}
               </FooterLinkText>
-            </ChakraLink>
-            <ChakraLink href='https://github.com/openaudiosearch/openaudiosearch' isExternal>
-              <Flex>
-               <FooterLinkText>
-                  <Icon as={FaGithub} fontSize='lg' mr='2' />
-                  Source code
-               </FooterLinkText>
-              </Flex>
             </ChakraLink>
             <FooterLinkText>
               <Login>
                 <ChakraLink>
+                  <Icon as={FaLock} mr='2' />
                   {t('login', 'Login')}
                 </ChakraLink>
               </Login>
-             </FooterLinkText>
+            </FooterLinkText>
+            <ChakraLink to='/imprint' as={Link}>
+              <FooterLinkText>
+                <Icon as={FaInfoCircle} mr='2' />
+                {t('imprint', 'Imprint')}
+              </FooterLinkText>
+            </ChakraLink>
           </Flex>
         </Box>
       </Center>
