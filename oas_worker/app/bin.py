e7bb60d13903a5301a71cfdb861fbad20a37b149
Franz Heinzmann (Frando) <frando@unbiskant.org>
2021-11-04 00:23:23 +0100

    worker: finish rename task --> job

--- oas_worker/app/bin.py
+++ oas_worker/app/bin.py
@@ -9,7 +9,7 @@ from app.worker import worker, DASHBOARD_PORT
 
 def run_with_args(worker, args):
     log.info(f"start OAS worker (pid {os.getpid()})")
-    log.info(f"active jobs: {list(worker.tasks)}")
+    log.info(f"registered jobs: {list(worker.jobs)}")
     try:
         if args.single is not None:
             worker.single(args.single)
@@ -26,10 +26,10 @@ def run_with_args(worker, args):
 
 def run(worker):
     description = "Open Audio Search worker"
-    epilog = f"Registered jobs: {list(worker.tasks)}"
+    epilog = f"Registered jobs: {list(worker.jobs)}"
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
     parser.add_argument('--single', type=str, help='Run a single job of this typ', metavar="JOB")
-    #  parser.add_argument('--dashboard', action='store_true', help='Enable the task dashboard on port {DASHBOARD_PORT}')
+    #  parser.add_argument('--dashboard', action='store_true', help='Enable the job dashboard on port {DASHBOARD_PORT}')
     args = parser.parse_args()
     run_with_args(worker, args)
 
--- oas_worker/app/worker.py
+++ oas_worker/app/worker.py
@@ -17,35 +17,35 @@ DASHBOARD_PORT = 8765
 class Worker(object):
     def __init__(self, config=config):
         self.config = config
-        self.tasks: Dict[str, Any] = {}
+        self.jobs: Dict[str, Any] = {}
         self.pool = None
         self.client = JobClient(self.config)
 
-    def task(self, name=None, default_concurrency=1):
+    def job(self, name=None, default_concurrency=1):
         """
-        Register a task
+        Register a job
         """
         def decorator(fn):
-            if name in self.tasks:
-                raise Exception(f"Task {name} already registered, cannot register a task with the same name twice.")
-            task_fn = TaskFn(name, fn, concurrency=default_concurrency)
-            self.tasks[name] = task_fn
-            return task_fn
+            if name in self.jobs:
+                raise Exception(f"Job {name} already registered, cannot register a job with the same name twice.")
+            job_fn = JobFn(name, fn, concurrency=default_concurrency)
+            self.jobs[name] = job_fn
+            return job_fn
 
         return decorator
 
 
     def start(self):
-        if not self.tasks:
+        if not self.jobs:
             raise RuntimeError("No jobs registered.")
-        tasks = []
-        for (name, task) in self.tasks.items():
-            for i in range(task.concurrency):
-                tasks.append((task.name, i))
-        n_jobs = len(tasks)
+        jobs = []
+        for (name, job) in self.jobs.items():
+            for i in range(job.concurrency):
+                jobs.append((job.name, i))
+        n_jobs = len(jobs)
         with WorkerPool(n_jobs=n_jobs, shared_objects=self) as pool:
             self.pool = pool
-            pool.map(work_loop, tasks)
+            pool.map(work_loop, jobs)
 
 
     def single(self, typ):
@@ -63,7 +63,7 @@ class Worker(object):
 
 
 def work_fn(worker, typ, i=0, ctx=None):
-    task = worker.tasks[typ]
+    job = worker.jobs[typ]
     config = worker.config
     if ctx is None:
         ctx = Context(worker, typ, i)
@@ -81,7 +81,7 @@ def work_fn(worker, typ, i=0, ctx=None):
 
         start = time.perf_counter()
         try:
-            result = task.fn(ctx, args, opts)
+            result = job.fn(ctx, args, opts)
             duration = round(time.perf_counter() - start, 3)
             worker.client.set_completed(job_id, patches=result.get("patches"), meta=result.get("meta"), duration=duration)
             ctx.log.info(f"completed job `{typ}:{job_id}` in {duration}s")
@@ -142,7 +142,7 @@ class Context(object):
         self.job = job
 
 
-class TaskFn(object):
+class JobFn(object):
     def __init__(self, name, fn, concurrency=1):
         self.name = name
         self.concurrency = concurrency
