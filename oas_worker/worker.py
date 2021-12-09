b83dd15c093e0241821c3c5eaa1d215f031142c1
dryptomim <moreiras@informatik.uni-freiburg.de>
2021-12-09 11:25:27 +0100

    download recasepunc model and recasepunc job

--- oas_worker/worker.py
+++ oas_worker/worker.py
@@ -4,7 +4,8 @@ from app.worker import worker
 from app.bin import run
 
 import app.jobs.jobs
-
+import app.jobs.recasepunc
+from app.jobs.recasepunc import WordpieceTokenizer 
 if __name__ == '__main__':
     run(worker)
 
