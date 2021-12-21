23e64957e92555c3c64c4d0e79c2cf7af98aabe7
Henning Schumann <henning.schumann@gmail.com>
2021-12-21 12:15:29 +0100

    Changed Job page to a table view, added details drawer and refresh button

--- frontend/src/pages/jobs.jsx
+++ frontend/src/pages/jobs.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import ReactJson from 'react-json-view'
 import useSWR from 'swr'
-import { Flex, Stack, Box, Text, Heading, IconButton, Input, Button, useDisclosure, Link, FormControl, Select, FormLabel, Spinner, AlertIcon, Alert } from '@chakra-ui/react'
+import { Flex, Stack, Box, Text, Heading, IconButton, Input, Button, useDisclosure, Link, FormControl, Select, FormLabel, Spinner, AlertIcon, Alert, 
+         Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption,
+         Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton} from '@chakra-ui/react'
 import {
   FaEdit as EditIcon,
-  FaCheck as SaveIcon
+  FaCheck as SaveIcon,
+  FaSync as RefreshIcon
 } from 'react-icons/fa'
 import { useForm } from 'react-hook-form'
 
@@ -17,11 +20,8 @@ export default function JobPage (props) {
       <Heading mb='2'>Jobs</Heading>
       <ImportUrl onJobSubmit={setSelectedJobId} />
       <Flex>
-        <Box w={['100%', '30%']}>
+        <Box w={['100%']}>
           <JobList onSelect={setSelectedJobId} selected={selectedJobId} />
-        </Box>
-        <Box w={['100%', '70%']}>
-          {selectedJobId && <Job id={selectedJobId} />}
         </Box>
       </Flex>
     </Stack>
@@ -85,28 +85,87 @@ function ImportUrl (props) {
 
 function JobList (props) {
   const { onSelect, selected } = props
-  const { data, error } = useSWR('/jobs', { refreshInterval: 1000 })
-  // console.debug('JobList', { data, error })
+  const { data, error, mutate } = useSWR('/jobs')
   if (error) return <Error error={error} />
   if (!data) return <Loading />
   console.log('JOBS', data)
   return (
-    <Box>
-      {data.map(job => (
-        <Box key={job.id} my='2' _hover={{ textDecoration: 'underline' }} bg={selected === job.id ? 'blue.300' : undefined}>
-          <Link onClick={e => onSelect(job.id)}>{job.id}</Link>
-        </Box>
-      ))}
+    <Box mt={2}>
+      <Flex direction='row-reverse'>
+        <Button onClick={() => mutate()} leftIcon={<RefreshIcon />}>Refresh</Button>
+      </Flex>
+      <Table variant='striped'>
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Status</Th>
+            <Th>Created at</Th>
+            <Th>Queue</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map(job => (
+            <Job key={job.id} job={job} />
+          ))}
+        </Tbody>
+      </Table>
     </Box>
   )
 }
 
 function Job (props) {
+  const { job } = props
+
+  return (
+    <Tr>
+      <Td>{job.id}</Td>
+      <Td>{job.status}</Td>
+      <Td>{job.created_at}</Td>
+      <Td>{job.queue}</Td>
+      <Td><JobDetails job={job} /></Td>
+    </Tr>
+  )
+}
+
+function JobDetails (props) {
+  const { job } = props
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+  return (
+    <>
+      <Button ref={btnRef} onClick={onOpen}>
+        Details
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Details for Job {job.id}</DrawerHeader>
+
+          <DrawerBody>
+            <JobJson id={job.id} />
+          </DrawerBody>
+
+          <DrawerFooter>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+function JobJson (props) {
   const { id } = props
-  const { data, error } = useSWR('/job/' + id, { refreshInterval: 1000 })
+  const { data, error } = useSWR('/job/' + id)
   if (error) return <Error error={error} />
   if (!data) return <Loading />
-  return <ReactJson src={data} collapsed='3' />
+    return <ReactJson src={data} collapsed='3' />
 }
 
 function Error (props) {
