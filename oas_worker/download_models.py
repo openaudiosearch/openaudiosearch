62a98f244220783edefb256bf16777f554bb6e51
Ewald Enzinger <ewald.enzinger@entn.at>
2022-01-11 18:10:25 -0500

    Add functions for downloading segmentation and diarization models

--- oas_worker/download_models.py
+++ oas_worker/download_models.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
-import wget
 import zipfile
+import tarfile
 import shutil
 import tempfile
 
@@ -23,6 +23,8 @@ def extract(filepath):
         
 def download_all_models():
     download_vosk_models()
+    download_diarization_models()
+    download_segmentation_models()
     download_spacy_models()
 
 
@@ -68,6 +70,56 @@ def download_vosk_models():
             extract(target_filepath)
         else:
             print(f'Skipping {models[model]}')
+
+
+def download_segmentation_models():
+    model_url = 'http://kaldi-asr.org/models/4/0004_tdnn_stats_asr_sad_1a.tar.gz'
+    target_dir = os.path.join(config.storage_path, "models", "segmentation")
+    if not os.path.isdir(target_dir):
+        os.makedirs(target_dir)
+    target_filepath = os.path.join(config.storage_path, "models", "0004_tdnn_stats_asr_sad_1a.tar.gz")
+    download(model_url, target_filepath)
+    with tarfile.open(target_filepath, 'r:gz') as tar_file:
+        with tar_file.extractfile('exp/segmentation_1a/tdnn_stats_asr_sad_1a/final.raw') as src_file, \
+            open(os.path.join(target_dir, "final.raw"), 'wb') as dest_file:
+            dest_file.write(src_file.read())
+        with tar_file.extractfile('exp/segmentation_1a/tdnn_stats_asr_sad_1a/post_output.vec') as src_file, \
+            open(os.path.join(target_dir, "post_output.vec"), 'wb') as dest_file:
+            dest_file.write(src_file.read())
+
+
+def download_diarization_models():
+    """Download diarization models and prepare model folder structure"""
+    model_url = 'http://kaldi-asr.org/models/6/0006_callhome_diarization_v2_1a.tar.gz'
+    target_dir = os.path.join(config.storage_path, "models", "diarization")
+    if not os.path.isdir(target_dir):
+        os.makedirs(target_dir)
+    target_filepath = os.path.join(config.storage_path, "models", "0006_callhome_diarization_v2_1a.tar.gz")
+    download(model_url, target_filepath)
+    with tarfile.open(target_filepath, 'r:gz') as tar_file:
+        with tar_file.extractfile('0006_callhome_diarization_v2_1a/exp/xvector_nnet_1a/extract.config') as src_file, \
+            open(os.path.join(target_dir, "extract.config"), 'wb') as dest_file:
+            dest_file.write(src_file.read())
+        with tar_file.extractfile('0006_callhome_diarization_v2_1a/exp/xvector_nnet_1a/final.raw') as src_file, \
+            open(os.path.join(target_dir, "final.raw"), 'wb') as dest_file:
+            dest_file.write(src_file.read())
+        # Prepare model for speaker embedding (xvector) extraction
+        p = subprocess.Popen([
+            "/kaldi/src/nnet3bin/nnet3-copy",
+            f"--nnet-config={target_dir}/extract.config",
+            f"{target_dir}/final.raw",
+            f"{target_dir}/extract.raw"
+        ], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        p.wait()
+        with tar_file.extractfile('0006_callhome_diarization_v2_1a/exp/xvector_nnet_1a/xvectors_callhome1/plda') as src_file, \
+            open(os.path.join(target_dir, "plda"), 'wb') as dest_file:
+            dest_file.write(src_file.read())
+        with tar_file.extractfile('0006_callhome_diarization_v2_1a/exp/xvector_nnet_1a/xvectors_callhome1/transform.mat') as src_file, \
+            open(os.path.join(target_dir, "transform.mat"), 'wb') as dest_file:
+            dest_file.write(src_file.read())
+        with tar_file.extractfile('0006_callhome_diarization_v2_1a/exp/xvector_nnet_1a/xvectors_callhome1/mean.vec') as src_file, \
+            open(os.path.join(target_dir, "mean.vec"), 'wb') as dest_file:
+            dest_file.write(src_file.read())
 
 
 if __name__ == "__main__":
